279e786ba0b558155faf5ad15af8d92b088b9858
MeztliRA <MeztliRA@gmail.com>
2021-04-21 12:14:36 +0700

    fixed morocco page

---
+++ pages/country/morocco.tsx
@@ -0,0 +1,13 @@
+import CountryInfo from "../../components/CountryInfo";
+
+export default function morocco() {
+    return (
+        <CountryInfo 
+            name="Morroco"
+            language="Arabic, Berber"
+            continent="Africa"
+            population="37,112,080 million"
+            currency="Morrocan dirham"
+        />
+    )
+}
