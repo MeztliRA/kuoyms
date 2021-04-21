8668dd1080532afa45e8ecd4b8e6e32fe1d0964f
MeztliRA <MeztliRA@gmail.com>
2021-04-21 09:10:55 +0700

    Add united kingdom to website

---
+++ pages/country/united-kingdom.tsx
@@ -0,0 +1,13 @@
+import CountryInfo from "../../components/CountryInfo";
+
+export default function united-kingdom() {
+    return (
+        <CountryInfo 
+            name="United Kingdom"
+            language="English"
+            continent="Europe"
+            population="63,182,178 million"
+            currency="Poundsterling"
+        />
+    )
+}
