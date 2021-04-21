5a993f7438f6f6106ab5fe88518390ce0e10d87a
MeztliRA <MeztliRA@gmail.com>
2021-04-21 12:32:55 +0700

    fixed typo

--- pages/country/morocco.tsx
+++ pages/country/morocco.tsx
@@ -3,11 +3,11 @@ import CountryInfo from "../../components/CountryInfo";
 export default function morocco() {
     return (
         <CountryInfo 
-            name="Morroco"
+            name="Morocco"
             language="Arabic, Berber"
             continent="Africa"
             population="37,112,080 million"
-            currency="Morrocan dirham"
+            currency="Moroccan dirham"
         />
     )
 }
