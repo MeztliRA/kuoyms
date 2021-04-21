b4b1684c4259f549438226ecda7ad3ee01203272
MeztliRA <MeztliRA@gmail.com>
2021-04-21 10:13:23 +0700

    Add canada page

---
+++ pages/country/canada.tsx
@@ -0,0 +1,13 @@
+import CountryInfo from "../../components/CountryInfo";
+
+export default function canada() {
+    return (
+        <CountryInfo 
+            name="Canada"
+            language="English, French"
+            continent="North America"
+            population="38,048,738 million"
+            currency="Canadian dollar"
+        />
+    )
+}
