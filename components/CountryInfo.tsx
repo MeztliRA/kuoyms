59ac294a9d3c0b41dea91cb343738c85f5542cdc
MeztliRA <MeztliRA@gmail.com>
2021-04-22 07:04:31 +0700

    Fix mobile page

--- components/CountryInfo.tsx
+++ components/CountryInfo.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Footer from "./Footer";
+import { useMediaQuery } from "react-responsive";
 import styles from "../styles/CountryInfo.module.css";
 
 interface Props {
@@ -11,11 +12,41 @@ interface Props {
 }
 
 export default function CountryInfo(props:Props) {
-    return (
+    const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
+    return isMobile ? (
         <>
             <div className={styles.container}>
             <Head>
                 <title>{props.name}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    {props.name}
+                    <br/>
+                    <br/>
+                </h1>
+                <div id="outer">
+                    <div id="inner">
+                    <p className={styles.card}>language:<br/><br/>{props.language}</p>
+                    <p className={styles.card}>population:<br/><br/>{props.population}</p>
+                    <p className={styles.card}>currency:<br/><br/>{props.currency}</p>
+                    <p className={styles.card}>continent:<br/><br/>{props.continent}</p>
+                    </div>
+                </div>
+            </main>
+
+            <Footer/>
+            </div>
+        </>
+    ) : (
+        <>
+            <div className={styles.container}>
+            <Head>
+                <title>{props.name}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Head>
 
             <main className={styles.main}>
